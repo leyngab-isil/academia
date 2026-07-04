d4360591cb409cc32dc275d9c83fa124eb643703
leyngab-isil <leyngab@isil.pe>
2026-07-04 09:21:23 -0500

    fix: home

--- assets/js/head.js
+++ assets/js/head.js
@@ -10,17 +10,22 @@ document.addEventListener("DOMContentLoaded", async () => {
         <title>Academia Ficticia - Inicio</title>
         <link rel="stylesheet" href="${base}assets/css/menu.css">
         <link rel="stylesheet" href="${base}assets/css/style.css">
+        <link rel="stylesheet" href="${base}assets/css/swiper.css">
         <link rel="stylesheet" href="${base}assets/bootstrap/css/bootstrap.min.css">
         <link rel="stylesheet" href="${base}assets/bootstrap/css/bootstrap.rtl.min.css">
         <link rel="stylesheet" href="${base}assets/bootstrap/css/bootstrap.min.css.map">
         <link rel="stylesheet" href="${base}assets/bootstrap/css/bootstrap.rtl.min.css.map">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.1/css/all.min.css"
-        integrity="sha512-2SwdPD6INVrV/lHTZbO2nodKhrnDdJK9/kg2XD1r9uGqPo1cUbujc+IYdlYdEErWNu69gVcYgdxlmVmzTWnetw=="
-        crossorigin="anonymous" referrerpolicy="no-referrer" />
+            integrity="sha512-2SwdPD6INVrV/lHTZbO2nodKhrnDdJK9/kg2XD1r9uGqPo1cUbujc+IYdlYdEErWNu69gVcYgdxlmVmzTWnetw=="
+            crossorigin="anonymous" referrerpolicy="no-referrer" />
         <link
             rel="stylesheet"
             href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"
-        />
+            />
+        <link
+            rel="stylesheet"
+            href="https://cdn.jsdelivr.net/npm/swiper@12/swiper-bundle.min.css"
+            />
     `
 
     document.getElementById("head").innerHTML = head
